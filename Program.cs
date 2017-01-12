594ed2510084337a5de3b93f8ecf5fa7ee35eaef
ksindorf000 <ksindorf000@gmail.com>
2017-01-11 23:28:53 -0500

    Win Logic isn't working :(

--- Program.cs
+++ Program.cs
@@ -8,42 +8,54 @@ namespace TicTacToe_HW2
 {
     class Program
     {
-        /*static string user1;
+        static string user1;
         static string user2;
-        static string[] gameBoard*/
+        static string[] gameBoard = { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
+        static string player;
+        static int winner = 0;
+        static int turnCount = 0;
+
 
         static void Main(string[] args)
         {
 
             /** Get user names, assign to X or O **/
             Console.WriteLine("User 1, what is your name? ");
-            string user1 = Console.ReadLine().ToUpper();
+            user1 = Console.ReadLine().ToUpper();
             Console.WriteLine($"{user1} you will be Xs.");
             Console.WriteLine("");
 
             Console.WriteLine("User2, what is your name? ");
-            string user2 = Console.ReadLine().ToUpper();
+            user2 = Console.ReadLine().ToUpper();
             Console.WriteLine($"{user2} you will be Os.");
             Console.ReadLine();
             Console.Clear();
 
-            /** Create Game Board Array **/
-            string[] gameBoard = { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
-
-            bool winner = false;
-            string player = user1;
+            player = user1;
 
             /** Play Game **/
-            while (!winner)
+            while (winner == 0)
             {
-                DisplayBoard(gameBoard);
-                winner = PlayerTurn(player, user1, user2, gameBoard);
+                DisplayBoard();
+                winner = PlayerTurn();
             }
 
+            if (winner != 3)
+            {
+                Console.Clear();
+                DisplayBoard();
+                Console.WriteLine($"{player} WINS!!!");
+            }
+            else
+            {
+                Console.Clear();
+                DisplayBoard();
+                Console.WriteLine("It's a draw!");
+            }
         }
 
         /** Method for displaying Game Board **/
-        public static void DisplayBoard(string[] gameBoard)
+        public static void DisplayBoard()
         {
 
             int counter = 0;
@@ -69,36 +81,28 @@ namespace TicTacToe_HW2
         }
 
         /* Method for user turn */
-        public static bool PlayerTurn(string player, string user1, string user2, string[] gameBoard)
+        public static int PlayerTurn()
         {
             int userMove;
             bool valid;
-            bool winner = false;
-            int turnCount = 0;
-            //string player;
 
-            //if (turnCount == 0)
-            //{
-            //    player = user1;
-            //}
+            turnCount++;
 
             Console.WriteLine($"{player}, where would you like to move?");
             userMove = int.Parse(Console.ReadLine());
 
-            valid = Validation(userMove, gameBoard, player, user1);
+            valid = Validation(userMove);
+            Console.WriteLine(gameBoard[2] + gameBoard[4] + gameBoard[6]);
+            
+            winner = WinConditions();
 
-            if (turnCount >= 5)
-            {
-                winner = WinConditions(gameBoard);
-            }
-
-            if (valid && !winner)
+            if (valid && winner == 0)
             {
                 /* Switch player */
                 player = player == user1 ? user2 : user1;
                 Console.Clear();
             }
-            else if (!valid && !winner)
+            else if (!valid && winner == 0)
             {
                 Console.WriteLine("Sorry, that's not a valid choice.");
                 Console.ReadLine();
@@ -110,8 +114,7 @@ namespace TicTacToe_HW2
         }
 
         /* Method for checking validity and assigning userMove to array */
-        public static bool Validation(int userMove, string[] gameBoard,
-            string user1, string player)
+        public static bool Validation(int userMove)
         {
             bool valid = true;
             string xo = player == user1 ? "X" : "O";
@@ -139,58 +142,64 @@ namespace TicTacToe_HW2
 
 
         /** Method for checking win conditions **/
-        public static bool WinConditions(string[] gameBoard)
+        public static int WinConditions()
         {
-            int i;
+            int i = 0;
             bool keepChecking = true;
-            bool winner = false;
 
             while (keepChecking)
             {
                 /* Check for vertical wins */
-                for (i = 0; i < 3; i++)
-                {
-                    if (gameBoard[i] == gameBoard[i + 3] && gameBoard[i] == gameBoard[i + 6])
-                    {
-                        winner = true;
-                        keepChecking = false;
-                        i = 3;
-                    }
-                }
-            }
 
-            while (keepChecking)
-            {
+                if (gameBoard[i] == gameBoard[i + 3] && gameBoard[i] == gameBoard[i + 6])
+                {
+                    winner = player == user1 ? 1 : 2;
+                    keepChecking = false;
+                }
+
                 /* Check for horizontal wins */
-                for (i = 0; i < 3; i++)
+
+                else if (gameBoard[i] == gameBoard[i + 1] && gameBoard[i] == gameBoard[i + 1])
                 {
-                    if (gameBoard[i] == gameBoard[i + 1] && gameBoard[i] == gameBoard[i + 1])
+                    winner = player == user1 ? 1 : 2;
+                    keepChecking = false;
+                }
+
+                /* Check for diagonal wins */
+
+                else if (gameBoard[i] == gameBoard[i + 4] && gameBoard[i] == gameBoard[i + 8])
+                {
+                    winner = player == user1 ? 1 : 2;
+                    keepChecking = false;
+                }
+                else if (gameBoard[i + 2] == gameBoard[i + 4] && gameBoard[i] == gameBoard[i + 6])
+                {
+                    winner = player == user1 ? 1 : 2;
+                    keepChecking = false;
+                }
+
+                /* Check for a tie */
+                else if (turnCount == 9)
+                {
+                    for (i = 0; i < 9; i++)
                     {
-                        winner = true;
-                        keepChecking = false;
-                        i = 3;
+                        if (gameBoard[i] == "X" || gameBoard[i] == "O")
+                        {
+                            winner = 3;
+                            keepChecking = false;
+                        }
+                        else
+                        {
+                            winner = 0;
+                            keepChecking = false;
+                        }
                     }
                 }
+
+                keepChecking = false;
+
             }
 
-            while (keepChecking)
-            {
-                /* Check for diagonal wins */
-                for (i = 0; i < 3; i++)
-                {
-                    if (gameBoard[i] == gameBoard[i + 4] && gameBoard[i] == gameBoard[i + 8])
-                    {
-                        winner = true;
-                        keepChecking = false;
-                    }
-                    else if (gameBoard[i + 2] == gameBoard[i + 4] && gameBoard[i] == gameBoard[i + 6])
-                    {
-                        winner = true;
-                        keepChecking = false;
-                    }
-                }
-            }
-            
             return winner;
 
         }
